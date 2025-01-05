5e3c78767341b48eb2f1b5505d96a81d89055032
Janith Chathuranga <127806197+janithScript@users.noreply.github.com>
2025-01-06 02:10:04 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import './App.css';
+import gptLogo from './assets/chatgpt.svg';
+import addBtn from './assets/add-30.png';
+import msgIcon from './assets/message.svg';
+import home from './assets/home.svg';
+import saved from './assets/bookmark.svg';
+import rocket from './assets/rocket.svg';
+import send from './assets/send.svg';
+import userIcon from './assets/user-icon.png';
+import gptImgLogo from './assets/chatgptLogo.svg';
+import { sendMsgToOpenAI } from './openai';
+import { useState } from 'react';
+
+function App() {
+
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([
+    {
+      text: "Hi, I am chatGPT. how can I help you?",
+      isBot: true,
+    }
+  ]);
+
+  const handleSend = async() => {
+    const res = await sendMsgToOpenAI(input)
+    setMessages([
+      ...messages,
+      { text: input, isBot: false },
+      { text: res, isBot: true }
+    ])
+  }
+  return (
+    <div className="App">
+      <div className='sideBar'>
+            <div className='upperSide'>
+              <div className='upperSideTop'><img src={gptLogo} alt="Logo" className="logo"/> <span className='brand'>ChatGPT</span> </div>
+              <button className='midBtn'><img src={addBtn} alt="new chat" className="addBtn"/>New Chat</button>
+              <div className="upperSideBottom">
+                <button className="query"><img src={msgIcon} alt="message"/>What is Programming ?</button> <br/>
+                <button className="query"><img src={msgIcon} alt="message"/>How to use an API ?</button>
+              </div>
+            </div>
+              
+            <div className='lowerSide'>
+                <div className="listItems"><img src={home} alt="Home" className="listitemsImg"/>Home</div>
+                <div className="listItems"><img src={saved} alt="Bookmark" className="listitemsImg"/>Saved</div>
+                <div className="listItems"><img src={rocket} alt="Upgrade" className="listitemsImg"/>Upgrade to pro</div>
+            </div>
+      </div>
+
+      <div className='main'>
+          <div className="chats">
+              {messages.map((message, i) => 
+                        <div key={i} className={message.isBot?"chat bot":"chat"}>
+                            <img className='chatImg' src={message.isBot?gptImgLogo:userIcon} alt="ChatGPT"/><p className="txt">{ message.text }</p>
+                        </div>
+              )}
+          </div>
+          <div className="chatFooter">
+            <div className="inp">
+              <input type="text" placeholder='Send a message' value={input} onChange={(e)=>{setInput(e.target.value)}}/><button className="send" onClick={handleSend}><img src={send} alt="Send"/></button>
+            </div>
+            <p>ChatGpt may provide incorrect results. Just double check the result.</p>
+          </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
